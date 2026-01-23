562554bf5af5dca56401798e3d3694da5aa2e27d
Benjamin Capetillo <capetillobenjamin85@gmail.com>
2026-01-23 06:56:05 -0500

    Update terraform to match new AWS configuration

--- api/terraform/prod/ec2.tf
+++ api/terraform/prod/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my_ec2" {
   ami           = "ami-00e428798e77d38d9"
-  instance_type = "t3.micro"
+  instance_type = "t3a.medium"
   subnet_id     = "subnet-091a8716cb204b874"
   vpc_security_group_ids = [
     aws_security_group.instance_sg.id, # allow ALB -> instance traffic
@@ -11,7 +11,7 @@ resource "aws_instance" "my_ec2" {
 
   root_block_device {
     volume_type = "gp3"
-    volume_size = 8
+    volume_size = 50
     iops        = 3000
     throughput  = 125
   }
@@ -20,4 +20,20 @@ resource "aws_instance" "my_ec2" {
     Name = "ais-api"
     Env  = var.env
   }
+}
+
+resource "aws_ebs_volume" "data_volume" {
+  availability_zone = "us-east-2c"
+  size              = 100
+  type              = "gp3"
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_volume_attachment" "data_volume_attachment" {
+  device_name = "/dev/sdb"
+  volume_id   = aws_ebs_volume.data_volume.id
+  instance_id = aws_instance.my_ec2.id
 }
